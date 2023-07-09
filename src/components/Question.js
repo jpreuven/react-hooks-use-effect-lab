db767ecab502261514f99b8536d89263edeacd51
Jonathan <jpreuven@gmail.com>
2023-07-09 09:34:56 -0700

    COMPLETED LAB

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining(timeRemaining - 1);
+      } else if (timeRemaining === 1) {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return function cleanup() {
+      // console.log(timeRemaining); //Ask why this is logging every time?
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
